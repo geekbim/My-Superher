3d5b08cdacf900c198e96b1cb1389d0abb55ad42
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-02-14 10:23:42 +0700

    My Superhero

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="#">Start Bootstrap</a>
+          <a class="navbar-brand" href="#">My Superhero</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -27,8 +27,8 @@ function App() {
       <header class="masthead text-center text-white">
         <div class="masthead-content">
           <div class="container">
-            <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-            <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+            <h1 class="masthead-heading mb-0">Superhero App</h1>
+            <h2 class="masthead-subheading mb-0">Kumpulan Superhero di Tata Surya</h2>
             <a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
           </div>
         </div>
