4dd5723c74a5ef0cead62c085bd8f7ae205314e1
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-02-16 10:10:24 +0700

    My Superhero

--- src/components/ListHeroes.jsx
+++ src/components/ListHeroes.jsx
@@ -1,7 +1,6 @@
-import imageOne from '../assets/img/01.jpg'
-import imageTwo from '../assets/img/02.jpg'
 import React, { useEffect, useState } from 'react'
 import { httpClient } from "../utils/api"
+import { ProgressBar } from 'react-bootstrap'
 
 export default function ListHeroes() {
 
@@ -47,8 +46,20 @@ export default function ListHeroes() {
                 <div className="col-sm-6 col-md-4 col-lg-3 my-4">
                     <div className="card">
                         <img className="card-img-top" src={ hero.images.sm } />
-                        <div className="card-block">
-                            <h5 className="text-bold p-2">{ hero.name }</h5>
+                        <div className="card-block p-2">
+                            <h5 className="text-bol">{ hero.name }</h5>
+                            <label>intelligence : </label>
+                            <ProgressBar now={ hero.powerstats.intelligence } label={ hero.powerstats.intelligence + "%" } variant="success" animated />
+                            <label>strength : </label>
+                            <ProgressBar now={ hero.powerstats.strength } label={ hero.powerstats.strength + "%" } variant="primary" animated />
+                            <label>speed : </label>
+                            <ProgressBar now={ hero.powerstats.speed } label={ hero.powerstats.speed + "%" } variant="danger" animated />
+                            <label>durability : </label>
+                            <ProgressBar now={ hero.powerstats.durability } label={ hero.powerstats.durability + "%" } variant="info" animated />
+                            <label>power : </label>
+                            <ProgressBar now={ hero.powerstats.power } label={ hero.powerstats.power + "%" } variant="secondary" animated />
+                            <label>combat : </label>
+                            <ProgressBar now={ hero.powerstats.combat } label={ hero.powerstats.combat + "%" } variant="dark" animated />
                         </div>
                     </div>
                 </div>
