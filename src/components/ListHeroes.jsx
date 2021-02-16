4ed4f5b6626ef75ba9aacfbfbe0a71338dbd9fc4
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-02-16 13:43:26 +0700

    My Superhero

--- src/components/ListHeroes.jsx
+++ src/components/ListHeroes.jsx
@@ -38,28 +38,69 @@ export default function ListHeroes() {
     }, [])
 
     /**
+     * render loading list superheroes
+     */
+    const renderLoadingListSuperheroes = () => {
+        return (
+            <div className="col-12 my-4 text-center">
+                <p>Loading...</p>
+            </div>
+        )
+    }
+
+    /**
      * render list superheroes
      */
     const renderListSuperheroes = () => {
         return heroes.map((hero, index) => {
             return (
-                <div className="col-sm-6 col-md-4 col-lg-3 my-4">
+                <div className="col-sm-6 col-md-4 col-lg-3 my-4" key={ index }>
                     <div className="card">
                         <img className="card-img-top" src={ hero.images.sm } />
                         <div className="card-block p-2">
                             <h5 className="text-bol">{ hero.name }</h5>
                             <label>intelligence : </label>
-                            <ProgressBar now={ hero.powerstats.intelligence } label={ hero.powerstats.intelligence + "%" } variant="success" animated />
+                            <ProgressBar 
+                                now={ hero.powerstats.intelligence } 
+                                label={ hero.powerstats.intelligence + "%" } 
+                                variant="success" 
+                                animated 
+                            />
                             <label>strength : </label>
-                            <ProgressBar now={ hero.powerstats.strength } label={ hero.powerstats.strength + "%" } variant="primary" animated />
+                            <ProgressBar 
+                                now={ hero.powerstats.strength } 
+                                label={ hero.powerstats.strength + "%" } 
+                                variant="primary" 
+                                animated 
+                            />
                             <label>speed : </label>
-                            <ProgressBar now={ hero.powerstats.speed } label={ hero.powerstats.speed + "%" } variant="danger" animated />
+                            <ProgressBar 
+                                now={ hero.powerstats.speed } 
+                                label={ hero.powerstats.speed + "%" } 
+                                variant="danger" 
+                                animated 
+                            />
                             <label>durability : </label>
-                            <ProgressBar now={ hero.powerstats.durability } label={ hero.powerstats.durability + "%" } variant="info" animated />
+                            <ProgressBar 
+                                now={ hero.powerstats.durability } 
+                                label={ hero.powerstats.durability + "%" } 
+                                variant="info" 
+                                animated 
+                            />
                             <label>power : </label>
-                            <ProgressBar now={ hero.powerstats.power } label={ hero.powerstats.power + "%" } variant="secondary" animated />
+                            <ProgressBar 
+                                now={ hero.powerstats.power } 
+                                label={ hero.powerstats.power + "%" } 
+                                variant="secondary" 
+                                animated 
+                            />
                             <label>combat : </label>
-                            <ProgressBar now={ hero.powerstats.combat } label={ hero.powerstats.combat + "%" } variant="dark" animated />
+                            <ProgressBar 
+                                now={ hero.powerstats.combat } 
+                                label={ hero.powerstats.combat + "%" } 
+                                variant="dark" 
+                                animated 
+                            />
                         </div>
                     </div>
                 </div>
@@ -70,7 +111,7 @@ export default function ListHeroes() {
     return (
         <div className="container">
             <div className="row">
-                { renderListSuperheroes() }
+                { heroes.length == 0 ? renderLoadingListSuperheroes() : renderListSuperheroes() }
             </div>
         </div>
     )
